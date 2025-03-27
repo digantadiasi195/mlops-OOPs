e13d669aa6324f10aa570c87fa39111ad0b0a857
digantadiasi195 <digantadiasi195>
2025-03-27 14:53:24 +0530

    First commit

---
+++ oops1.py
@@ -0,0 +1,11 @@
+class Employee:
+    def __init__(self):
+        self.id="sai"
+        self.salary=25
+        self.designation=25000
+
+
+sam = Employee()
+print(sam.id)
+print(sam.salary)
+print(sam.designation)
